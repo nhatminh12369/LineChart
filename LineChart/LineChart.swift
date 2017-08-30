d78473140c9f3c7886cca63c4bab4f3a8d07bdd4
Nguyen Vu Nhat Minh <Leonard@Nguyens-MacBook-Pro.local>
2017-08-30 11:43:12 +0700

    Finished LineChart.swift

--- LineChart/LineChart.swift
+++ LineChart/LineChart.swift
@@ -23,13 +23,13 @@ extension PointEntry: Comparable {
 }
 
 class LineChart: UIView {
-    let lineGap: CGFloat = 40.0
+    let lineGap: CGFloat = 60.0
     let topSpace: CGFloat = 40.0
     let bottomSpace: CGFloat = 40.0
     
     var isCurved: Bool = false
     
-    var dataEntries: [PointEntry] = [] {
+    var dataEntries: [PointEntry]? {
         didSet {
             self.setNeedsLayout()
         }
@@ -37,9 +37,11 @@ class LineChart: UIView {
     
     private let mainLayer: CALayer = CALayer()
     private let dataLayer: CALayer = CALayer()
+    private let gridLayer: CALayer = CALayer()
+    private let gradientLayer: CAGradientLayer = CAGradientLayer()
     private let scrollView: UIScrollView = UIScrollView()
     
-    private var dataPoints: [CGPoint] = []
+    private var dataPoints: [CGPoint]?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -59,19 +61,32 @@ class LineChart: UIView {
     private func setupView() {
         mainLayer.addSublayer(dataLayer)
         scrollView.layer.addSublayer(mainLayer)
+        
+        gradientLayer.colors = [#colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.7).cgColor, UIColor.clear.cgColor]
+        scrollView.layer.addSublayer(gradientLayer)
+        self.layer.addSublayer(gridLayer)
         self.addSubview(scrollView)
+        self.backgroundColor = #colorLiteral(red: 0, green: 0.3529411765, blue: 0.6156862745, alpha: 1)
     }
     
     override func layoutSubviews() {
         scrollView.frame = CGRect(x: 0, y: 0, width: self.frame.size.width, height: self.frame.size.height)
-        scrollView.contentSize = CGSize(width: CGFloat(dataEntries.count) * lineGap, height: self.frame.size.height)
-        mainLayer.frame = CGRect(x: 0, y: 0, width: CGFloat(dataEntries.count) * lineGap, height: self.frame.size.height)
-        dataLayer.frame = CGRect(x: 0, y: topSpace, width: mainLayer.frame.width, height: mainLayer.frame.height - topSpace - bottomSpace)
-        dataPoints = convertDataEntriesToPoints(entries: dataEntries)
-        if isCurved {
-            drawCurvedChart()
-        } else {
-            drawChart()
+        if let dataEntries = dataEntries {
+            scrollView.contentSize = CGSize(width: CGFloat(dataEntries.count) * lineGap, height: self.frame.size.height)
+            mainLayer.frame = CGRect(x: 0, y: 0, width: CGFloat(dataEntries.count) * lineGap, height: self.frame.size.height)
+            dataLayer.frame = CGRect(x: 0, y: topSpace, width: mainLayer.frame.width, height: mainLayer.frame.height - topSpace - bottomSpace)
+            gradientLayer.frame = dataLayer.frame
+            dataPoints = convertDataEntriesToPoints(entries: dataEntries)
+            gridLayer.frame = CGRect(x: 0, y: topSpace, width: self.frame.width, height: mainLayer.frame.height - topSpace - bottomSpace)
+            
+            drawHorizontalLines()
+            if isCurved {
+                drawCurvedChart()
+            } else {
+                drawChart()
+            }
+            maskGradientLayer()
+            drawTitles()
         }
     }
     
@@ -93,13 +108,10 @@ class LineChart: UIView {
     }
     
     private func drawChart() {
-        if (dataPoints.count > 0) {
-            let path = UIBezierPath()
-            path.move(to: dataPoints[0])
+        if let dataPoints = dataPoints,
+            dataPoints.count > 0,
+            let path = createPath() {
             
-            for i in 1..<dataPoints.count {
-                path.addLine(to: dataPoints[i])
-            }
             let lineLayer = CAShapeLayer()
             lineLayer.path = path.cgPath
             lineLayer.strokeColor = UIColor.black.cgColor
@@ -108,22 +120,137 @@ class LineChart: UIView {
         }
     }
     
+    private func createPath() -> UIBezierPath? {
+        guard let dataPoints = dataPoints, dataPoints.count > 0 else {
+            return nil
+        }
+        let path = UIBezierPath()
+        path.move(to: dataPoints[0])
+        
+        for i in 1..<dataPoints.count {
+            path.addLine(to: dataPoints[i])
+        }
+        return path
+    }
+    
     private func drawCurvedChart() {
-        if (dataPoints.count > 0) {
-            let path = UIBezierPath()
-            path.move(to: dataPoints[0])
-            
-            var curveSegments: [CurveSegment] = []
-            curveSegments = CurveAlgorithm.shared.controlPointsFrom(points: dataPoints)
-            
-            for i in 1..<dataPoints.count {
-                path.addCurve(to: dataPoints[i], controlPoint1: curveSegments[i-1].controlPoint1, controlPoint2: curveSegments[i-1].controlPoint2)
-            }
+        guard let dataPoints = dataPoints, dataPoints.count > 0 else {
+            return
+        }
+        if let path = createCurvedPath() {
             let lineLayer = CAShapeLayer()
             lineLayer.path = path.cgPath
-            lineLayer.strokeColor = UIColor.black.cgColor
+            lineLayer.strokeColor = UIColor.white.cgColor
             lineLayer.fillColor = UIColor.clear.cgColor
             dataLayer.addSublayer(lineLayer)
+        }
+    }
+    
+    private func createCurvedPath() -> UIBezierPath? {
+        guard let dataPoints = dataPoints, dataPoints.count > 0 else {
+            return nil
+        }
+        let path = UIBezierPath()
+        path.move(to: dataPoints[0])
+        
+        var curveSegments: [CurveSegment] = []
+        curveSegments = CurveAlgorithm.shared.controlPointsFrom(points: dataPoints)
+        
+        for i in 1..<dataPoints.count {
+            path.addCurve(to: dataPoints[i], controlPoint1: curveSegments[i-1].controlPoint1, controlPoint2: curveSegments[i-1].controlPoint2)
+        }
+        return path
+    }
+    
+    private func maskGradientLayer() {
+        if let dataPoints = dataPoints,
+            dataPoints.count > 0 {
+            
+            let path = UIBezierPath()
+            path.move(to: CGPoint(x: dataPoints[0].x, y: dataLayer.frame.height))
+            path.addLine(to: dataPoints[0])
+            if let curvedPath = createCurvedPath() {
+                path.append(curvedPath)
+            }
+            path.addLine(to: CGPoint(x: dataPoints[dataPoints.count-1].x, y: dataLayer.frame.height))
+            path.addLine(to: CGPoint(x: dataPoints[0].x, y: dataLayer.frame.height))
+            
+            let maskLayer = CAShapeLayer()
+            maskLayer.path = path.cgPath
+            maskLayer.fillColor = UIColor.white.cgColor
+            maskLayer.strokeColor = UIColor.clear.cgColor
+            maskLayer.lineWidth = 0.0
+            
+            gradientLayer.mask = maskLayer
+        }
+    }
+    
+    private func drawTitles() {
+        if let dataEntries = dataEntries,
+            dataEntries.count > 0 {
+            for i in 0..<dataEntries.count {
+                let textLayer = CATextLayer()
+                textLayer.frame = CGRect(x: lineGap*CGFloat(i) - lineGap/2, y: mainLayer.frame.size.height - bottomSpace/2 - 8, width: lineGap, height: 16)
+                textLayer.foregroundColor = #colorLiteral(red: 0.5019607843, green: 0.6784313725, blue: 0.8078431373, alpha: 1).cgColor
+                textLayer.backgroundColor = UIColor.clear.cgColor
+                textLayer.alignmentMode = kCAAlignmentCenter
+                textLayer.contentsScale = UIScreen.main.scale
+                textLayer.font = CTFontCreateWithName(UIFont.systemFont(ofSize: 0).fontName as CFString, 0, nil)
+                textLayer.fontSize = 11
+                textLayer.string = dataEntries[i].title
+                mainLayer.addSublayer(textLayer)
+            }
+        }
+    }
+    
+    private func drawHorizontalLines() {
+        guard let dataEntries = dataEntries else {
+            return
+        }
+        gridLayer.sublayers?.forEach({$0.removeFromSuperlayer()})
+        
+        var gridValues: [CGFloat]? = nil
+        if dataEntries.count < 4 && dataEntries.count > 0 {
+            gridValues = [0, 1]
+        } else if dataEntries.count >= 4 {
+            gridValues = [0, 0.25, 0.5, 0.75, 1]
+        }
+        if let gridValues = gridValues {
+            for value in gridValues {
+                let height = value * gridLayer.frame.size.height
+                
+                let path = UIBezierPath()
+                path.move(to: CGPoint(x: 0, y: height))
+                path.addLine(to: CGPoint(x: gridLayer.frame.size.width, y: height))
+                
+                let lineLayer = CAShapeLayer()
+                lineLayer.path = path.cgPath
+                lineLayer.fillColor = UIColor.clear.cgColor
+                lineLayer.strokeColor = #colorLiteral(red: 0.2784313725, green: 0.5411764706, blue: 0.7333333333, alpha: 1).cgColor
+                lineLayer.lineWidth = 0.5
+                if (value > 0.0 && value < 1.0) {
+                    lineLayer.lineDashPattern = [4, 4]
+                }
+                
+                gridLayer.addSublayer(lineLayer)
+                
+                var minMaxGap = 0
+                if let max = dataEntries.max()?.value,
+                    let min = dataEntries.min()?.value {
+                    minMaxGap = (max - min) * 110 / 100
+                }
+                
+                let textLayer = CATextLayer()
+                textLayer.frame = CGRect(x: 4, y: height, width: 50, height: 16)
+                textLayer.foregroundColor = #colorLiteral(red: 0.5019607843, green: 0.6784313725, blue: 0.8078431373, alpha: 1).cgColor
+                textLayer.backgroundColor = UIColor.clear.cgColor
+                textLayer.contentsScale = UIScreen.main.scale
+                textLayer.font = CTFontCreateWithName(UIFont.systemFont(ofSize: 0).fontName as CFString, 0, nil)
+                textLayer.fontSize = 12
+                textLayer.string = "\(Int((1-value) * CGFloat(minMaxGap)))"
+                
+                gridLayer.addSublayer(textLayer)
+            }
         }
     }
 }
--- LineChart/ViewController.swift
+++ LineChart/ViewController.swift
@@ -15,8 +15,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        lineChart.dataEntries = [PointEntry(value: 0, title: "aa"), PointEntry(value: 100, title: "aa"), PointEntry(value: 100, title: "aa"), PointEntry(value: 300, title: "aa"), PointEntry(value: 500, title: "aa"), PointEntry(value: 200, title: "aa"),PointEntry(value: 50, title: "aa"),PointEntry(value: 300, title: "aa"),PointEntry(value: 600, title: "aa")]
+        self.view.backgroundColor = #colorLiteral(red: 0, green: 0.3529411765, blue: 0.6156862745, alpha: 1)
+        
+        //lineChart.dataEntries = [PointEntry(value: 0, title: "aa"), PointEntry(value: 100, title: "aa"), PointEntry(value: 100, title: "aa"), PointEntry(value: 300, title: "aa"), PointEntry(value: 500, title: "aa"), PointEntry(value: 200, title: "aa"),PointEntry(value: 50, title: "aa"),PointEntry(value: 300, title: "aa"),PointEntry(value: 600, title: "aa")]
+        lineChart.dataEntries = generateRandomEntries()
         lineChart.isCurved = true
+    }
+    
+    private func generateRandomEntries() -> [PointEntry] {
+        var result: [PointEntry] = []
+        for i in 0..<100 {
+            let value = Int(arc4random() % 500)
+            
+            let formatter = DateFormatter()
+            formatter.dateFormat = "d MMM"
+            var date = Date()
+            date.addTimeInterval(TimeInterval(24*60*60*i))
+            
+            result.append(PointEntry(value: value, title: formatter.string(from: date)))
+        }
+        return result
     }
 }
 
